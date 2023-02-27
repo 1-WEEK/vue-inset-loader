98610242c763583161316afff2f7298259565063
1-week <bayeszou@gmail.com>
2023-02-27 16:56:49 +0800

    feat: support ts & page-meta & remove label&config

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-
 # vue-inset-loader
-#### 编译阶段在sfc模板指定位置插入自定义内容，适用于webpack构建的vue应用，常用于小程序需要全局引入组件的场景。（由于小程序没有开放根标签，没有办法在根标签下追加全局标签，所以要使用组件必须在当前页面引入组件标签）
+
+仅支持 HTML 类型的 template
+
+#### 编译阶段在 sfc 模板指定位置插入自定义内容，适用于 webpack 构建的 vue 应用，常用于小程序需要全局引入组件的场景。（由于小程序没有开放根标签，没有办法在根标签下追加全局标签，所以要使用组件必须在当前页面引入组件标签）
 
 ### 第一步 安装
 
     npm install vue-inset-loader --save-dev
 
-### 第二步 vue.config.js注入loader
+### 第二步 vue.config.js 注入 loader
 
     module: {
         rules: [
@@ -25,15 +27,9 @@
         ]
     },
 
-### 第三步 pages.json配置文件中添加insetLoader
+### 第三步 pages.json 配置文件中添加 insetLoader
 
     "insetLoader": {
-        "config":{
-            "confirm": "<BaseConfirm ref='confirm'></BaseConfirm>",
-            "abc": "<BaseAbc ref='BaseAbc'></BaseAbc>"
-        },
-        // 全局配置
-        "label":["confirm"],
         "rootEle":"div"
     },
     "pages": [
@@ -42,19 +38,14 @@
             "style": {
                 "navigationBarTitleText": "测试页面",
                 // 单独配置，用法跟全局配置一致，优先级高于全局
-                "label": ["confirm","abc"],
                 "rootEle":"div"
             }
         },
     ]
 
-###  配置说明
+### 配置说明
 
- - `config` (default: `{}`)
-    定义标签名称和内容的键值对
- - `label`(default: `[]`)
-    需要全局引入的标签，打包后会在所有页面引入此标签
- - `rootEle`(default: `"div"`)
-    根元素的标签类型，缺省值为div，支持正则，比如匹配任意标签 ".*"
+- `rootEle`(default: `"div"`)
+  根元素的标签类型，缺省值为 div
 
- ✔ `label` 和 `rootEle` 支持在单独页面的style里配置，优先级高于全局配置
+✔ `rootEle` 支持在单独页面的 style 里配置，优先级高于全局配置
--- utils.js
+++ utils.js
@@ -1,105 +1,126 @@
-const fs = require('fs')
-const path = require('path')
-const stripJsonComments = require("../strip-json-comments")
+const fs = require("fs");
+const path = require("path");
+const stripJsonComments = require("strip-json-comments");
 
 // 反序列化后的pages.json对象
-let pagesJson = {}
+let pagesJson = {};
 // 此loader配置对象
-let insetLoader = {}
+let insetLoader = {};
 // pages.json文件所在目录
-let rootPath =  process.env.UNI_INPUT_DIR || (process.env.INIT_CWD + '\\src')
-
+let rootPath = process.env.UNI_INPUT_DIR || process.env.INIT_CWD + "\\src";
 
 // 在template中用正则匹配并替换一段代码
-const generateHtmlCode = (template,labelCode,regLabel)=>{
-	// 去除html所有注释和首尾空白
-	const regNotes = /<!--((?!(#ifdef|#ifndef|#endif)).|[\r\n])*?-->/g
-	const regBlank = /^\s+|\s+$/g
-	return template
-	.replace(regNotes,'')
-	.replace(regBlank,'')
-	.replace(regLabel,`${labelCode}$1`)
-}
+const generateHtmlCode = (template, labelCode, regLabel) => {
+  // 去除html所有注释和首尾空白
+  const regNotes = /<!--((?!(#ifdef|#ifndef|#endif)).|[\r\n])*?-->/g;
+  const regBlank = /^\s+|\s+$/g;
+  return template
+    .replace(regNotes, "")
+    .replace(regBlank, "")
+    .replace(regLabel, `${labelCode}$1`);
+};
 
 // 获取到需要插入的所有label标签
-const generateLabelCode = (labelArr) => labelArr.map(e => insetLoader.config[e] || '').join('')
+const generateLabelCode = (labelArr) =>
+  labelArr.map((e) => insetLoader.config[e] || "").join("");
 
 // 根据compiler组合成style标签字符串代码
-const generateStyleCode = (styles)=>styles.reduce((str,item,i)=>{
-	return str +=  `<style ${item.lang?("lang='"+item.lang+"'"):''} ${item.scoped?("scoped='"+item.scoped+"'"):''}>
+const generateStyleCode = (styles) =>
+  styles.reduce((str, item, i) => {
+    return (str += `<style ${item.lang ? "lang='" + item.lang + "'" : ""} ${
+      item.scoped ? "scoped='" + item.scoped + "'" : ""
+    }>
 		${item.content}
-	</style>`
-},'')
+	</style>`);
+  }, "");
 
 // 分析pages.json，生成路由和配置的映射对象
-const getPagesMap = ()=>{
-	// 获取主包路由配置
-	const pages = pagesJson.pages || []
-	const subpackages = pagesJson.subpackages || pagesJson.subPackages || []
-	return pages.reduce((obj,item)=>{
-        const curPage = getLabelConfig(item)
-        curPage.label && (obj['/'+item.path] = curPage)
-	    return obj
-	},subpackages.reduce((obj,item)=>{
-		// 获取分包路由配置
-	    const root = item.root
-	    item.pages.forEach((item)=>{
-	    	const curPage = getLabelConfig(item)
-	        curPage.label && (obj['/'+root+'/'+item.path] = curPage)
-	    })
-	    return obj
-	},{}))
-}
+const getPagesMap = () => {
+  // 获取主包路由配置
+  const pages = pagesJson.pages || [];
+  const subpackages = pagesJson.subpackages || pagesJson.subPackages || [];
+  return pages.reduce(
+    (obj, item) => {
+      const curPage = getLabelConfig(item);
+      obj["/" + item.path] = curPage;
+      return obj;
+    },
+    subpackages.reduce((obj, item) => {
+      // 获取分包路由配置
+      const root = item.root;
+      item.pages.forEach((item) => {
+        const curPage = getLabelConfig(item);
+        obj["/" + root + "/" + item.path] = curPage;
+      });
+      return obj;
+    }, {})
+  );
+};
 
 // 生成path对应的对象结构
-const getLabelConfig = (json)=>{
-	return {
-		label: (json.style && json.style.label) || insetLoader.label,
-		ele: (json.style && json.style.rootEle) || insetLoader.rootEle
-	}
-}
+const getLabelConfig = (json) => {
+  return {
+    label: (json.style && json.style.label) || insetLoader.label,
+    ele: (json.style && json.style.rootEle) || insetLoader.rootEle,
+  };
+};
 
 // 反序列化page.json并缓存，
 // 并根据page.json分析是否有效并且需要后续逻辑处理
-const initPages = (that)=>{
-	let pagesPath = (that.query || {}).pagesPath
-	if(!pagesPath){
-		// 默认读取pages.json
-		pagesPath = path.resolve(rootPath, 'pages.json')
-	}else{
-		// 如有传自定义pagesPath，则截取出所在目录作为rootPath，用于后续匹配路由
-		rootPath = path.resolve(pagesPath,'../')
-	}
-	pagesJson = JSON.parse(stripJsonComments(fs.readFileSync(pagesPath,'utf8')))
-	return initInsetLoader()
-}
-
+const initPages = (that) => {
+  let pagesPath = (that.query || {}).pagesPath;
+  if (!pagesPath) {
+    // 默认读取pages.json
+    pagesPath = path.resolve(rootPath, "pages.json");
+  } else {
+    // 如有传自定义pagesPath，则截取出所在目录作为rootPath，用于后续匹配路由
+    rootPath = path.resolve(pagesPath, "../");
+  }
+  pagesJson = JSON.parse(stripJsonComments(fs.readFileSync(pagesPath, "utf8")));
+  return initInsetLoader();
+};
 
 // 给非必填项设置缺省值，缺少主要对象返回false
-initInsetLoader = ()=>{
-	insetLoader = pagesJson.insetLoader || {}
-	// label：全局标签配置
-	// rootEle：根元素的类型,也支持正则,如匹配任意标签.*
-	insetLoader.label = insetLoader.label || []
-	insetLoader.rootEle = insetLoader.rootEle || "div"
+initInsetLoader = () => {
+  insetLoader = pagesJson.insetLoader || {};
+  // label：全局标签配置
+  // rootEle：根元素的类型,也支持正则,如匹配任意标签.*
+  insetLoader.rootEle = insetLoader.rootEle || "div";
 
-	// config对象为空视为无效配置
-	const effective = typeof insetLoader.config == 'object' && Object.keys(insetLoader.config).length
-	return effective
-}
+  return true;
+};
 
 // 根据resourcePath获取路由
-const getRoute = (resourcePath) => resourcePath
-.replace(rootPath,'')
-.replace('.vue','')
-.replace(/\\/g,'/')
+const getRoute = (resourcePath) =>
+  resourcePath.replace(rootPath, "").replace(".vue", "").replace(/\\/g, "/");
+
+/**
+ * 指定位置替换字符串
+ *
+ * @param {string} content
+ * @param {string} str
+ * @param {number} start
+ * @param {number} end
+ * @returns {string}
+ */
+function replaceByPosition(content, str, start, end) {
+  if (start >= end) return content;
+  if (end >= content.length) return content;
+
+  return [
+    content.substring(0, start),
+    str,
+    content.substring(end, content.length),
+  ].join("");
+}
 
 module.exports = {
-	generateHtmlCode,
-	generateLabelCode,
-	generateStyleCode,
-	initInsetLoader,
-	getPagesMap,
-	initPages,
-	getRoute
-}
+  generateHtmlCode,
+  generateLabelCode,
+  generateStyleCode,
+  initInsetLoader,
+  getPagesMap,
+  initPages,
+  getRoute,
+  replaceByPosition,
+};
